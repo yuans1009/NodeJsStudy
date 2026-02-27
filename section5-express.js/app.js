19fcb6b50e1d065115177dad7456c04a0cafbe61
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 20:26:43 +0800

    app.post(), app.get() instead of app.use, which called in all http methods.

--- section5-express.js/app.js
+++ section5-express.js/app.js
@@ -10,14 +10,14 @@ app.use('/add-product', (req, res, next) => {
   res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/products', (req, res, next) => {
+app.post('/products', (req, res, next) => {
    console.log(req.body); // undefined, because we haven't set up body parsing middleware yet
    res.redirect('/'); // redirect to the root path
 });
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  res.send('<h1>Hello from Express - GET!</h1>');
 });
 
 app.listen(3000);
