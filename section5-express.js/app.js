46117b2aa7cb4f276bd3739628c3b4c71da7df9a
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 20:14:58 +0800

    app.use([path,].

--- section5-express.js/app.js
+++ section5-express.js/app.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
+// https://expressjs.com/en/5x/api.html#app.use
+// app.use([path,] callback [, callback...])
+// path defaults to '/' if not specified. This means that the middleware function will be executed for every request to the app, regardless of the requested path. 
+// If a path is specified, the middleware function will only be executed for requests that match the specified path.
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log('In another middleware!');
-  // default content type is text/html, so we can send html content
+  res.send('<h1>Add Product Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
