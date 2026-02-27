cab6bb3f247ca65aa1b0565068ec3de4ee387437
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 20:19:15 +0800

    req.body is undefined if there is no body parsing middleware

--- section5-express.js/app.js
+++ section5-express.js/app.js
@@ -6,14 +6,15 @@ const app = express();
 // app.use([path,] callback [, callback...])
 // path defaults to '/' if not specified. This means that the middleware function will be executed for every request to the app, regardless of the requested path. 
 // If a path is specified, the middleware function will only be executed for requests that match the specified path.
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Add Product Page</h1>');
+  res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/products', (req, res, next) => {
+   console.log(req.body); // undefined, because we haven't set up body parsing middleware yet
+   res.redirect('/'); // redirect to the root path
 });
 
 app.use('/', (req, res, next) => {
