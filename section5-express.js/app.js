681e8610accc0edf0c00229b3d80aefd8d867724
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 20:48:26 +0800

    Put common starting path in app.use

--- section5-express.js/app.js
+++ section5-express.js/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // NOTE: the order of middleware matters if using app.use()! 
 // We need to set up body parsing middleware before we define our routes that need to access req.body
 // app.get() app.post() use exact path matching, while app.use() will match any path that starts with the given path (or all paths if no path is given). So if we use app.use() for our routes, we need to make sure to place it after the body parsing middleware, otherwise req.body will be undefined in our route handlers.
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // This will catch any requests that haven't been handled by the above routes and send a 404 response
--- section5-express.js/routes/admin.js
+++ section5-express.js/routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/admin/add-product', (req, res, next) => {
-  res.send('<form action="/admin/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+router.get('/add-product', (req, res, next) => {
+  res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-router.post('/admin/products', (req, res, next) => {
+router.post('/products', (req, res, next) => {
    console.log(req.body); // undefined, because we haven't set up body parsing middleware yet
    res.redirect('/'); // redirect to the root path
 });
