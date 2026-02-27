e96acef1eabf6bd12e6414b6ab56f707f6923681
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 20:36:43 +0800

    use express.Router() to routes. use get/post/... instead of use.

--- section5-express.js/app.js
+++ section5-express.js/app.js
@@ -3,21 +3,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// parse body sent by HTML forms
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/products', (req, res, next) => {
-   console.log(req.body); // undefined, because we haven't set up body parsing middleware yet
-   res.redirect('/'); // redirect to the root path
-});
-
-app.get('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express - GET!</h1>');
-});
+// NOTE: the order of middleware matters if using app.use()! 
+// We need to set up body parsing middleware before we define our routes that need to access req.body
+// app.get() app.post() use exact path matching, while app.use() will match any path that starts with the given path (or all paths if no path is given). So if we use app.use() for our routes, we need to make sure to place it after the body parsing middleware, otherwise req.body will be undefined in our route handlers.
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
---
+++ section5-express.js/routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+  res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+router.post('/products', (req, res, next) => {
+   console.log(req.body); // undefined, because we haven't set up body parsing middleware yet
+   res.redirect('/'); // redirect to the root path
+});
+
+module.exports = router;
