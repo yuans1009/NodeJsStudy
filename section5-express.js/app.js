2befad9b6dca982bcddc2849b053066cec384722
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 20:24:15 +0800

    parse form data using body-parser

--- section5-express.js/app.js
+++ section5-express.js/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// https://expressjs.com/en/5x/api.html#app.use
-// app.use([path,] callback [, callback...])
-// path defaults to '/' if not specified. This means that the middleware function will be executed for every request to the app, regardless of the requested path. 
-// If a path is specified, the middleware function will only be executed for requests that match the specified path.
-
+// parse body sent by HTML forms
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
   res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
