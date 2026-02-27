813376b3cc84133ec25e1aed17b55d0ac4a3e863
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 21:01:23 +0800

    Use res.sendFile to serve html files. Use path to find the html files.

--- section5-express.js/routes/admin.js
+++ section5-express.js/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 router.post('/products', (req, res, next) => {
--- section5-express.js/routes/shop.js
+++ section5-express.js/routes/shop.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express - GET!</h1>');
+   // '/' means the root path of OS.
+   // res.sendFile('/views/shop.html');
+
+   // __dirname is the directory of the current module (i.e. the directory of this shop.js file). 
+   // So we need to go up one level to get to the root directory of our project, and then navigate to the views folder and then to shop.html.
+   res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
