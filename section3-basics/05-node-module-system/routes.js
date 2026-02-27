0a27d89cf2b357e56f033da8ee87085bc37a7a93
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-02-27 16:01:41 +0800

    npm install nodemon --save-dev start script: nodemon app.js npm start

--- section3-basics/05-node-module-system/routes.js
+++ section3-basics/05-node-module-system/routes.js
@@ -6,6 +6,7 @@ const requestHandler = (req, res) => {
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
