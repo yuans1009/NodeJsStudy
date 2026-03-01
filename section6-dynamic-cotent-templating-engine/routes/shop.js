918ff10c2f1b482fe290839aaa6646a7f20a83a1
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-01 20:29:17 +0800

    Use pug to render dynamic content

--- section6-dynamic-cotent-templating-engine/routes/shop.js
+++ section6-dynamic-cotent-templating-engine/routes/shop.js
@@ -8,10 +8,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-//   const products = adminData.products;
-//   console.log(products);
-//   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-   res.render('shop');
+   const products = adminData.products;
+   res.render('shop', {  docTitle: 'Shop', prods: products });
 });
 
 module.exports = router;
