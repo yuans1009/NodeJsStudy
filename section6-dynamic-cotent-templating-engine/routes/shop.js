01f46f71dcb45b231b257a9b5a401f473c10ff3f
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-01 19:54:00 +0800

    use variable to store products (for now)

--- section6-dynamic-cotent-templating-engine/routes/shop.js
+++ section6-dynamic-cotent-templating-engine/routes/shop.js
@@ -3,10 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  console.log(products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
