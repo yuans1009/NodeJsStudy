0c44d96f4f165cf2c072a78c36c627772184bfd2
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-01 20:59:01 +0800

    Convert shop.pug to use layout. finishing

--- section6-dynamic-cotent-templating-engine/routes/shop.js
+++ section6-dynamic-cotent-templating-engine/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
    const products = adminData.products;
-   res.render('shop', {  docTitle: 'Shop', prods: products });
+   res.render('shop', { pageTitle: 'Shop', prods: products, path: '/' });
 });
 
 module.exports = router;
