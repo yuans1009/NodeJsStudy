7314a43b6c5d5866418420cb3585d3e4fd918bbb
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-08 10:45:49 +0800

    Switch to ejs template engine

--- section6-dynamic-cotent-templating-engine/app.js
+++ section6-dynamic-cotent-templating-engine/app.js
@@ -2,15 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// set up express-handlebars as templating engine
-// the name should be the same as the extension of the template files
-// Set main-layout.hbs as the default layout, so we don't have to specify it in every render() call
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // views is the default value, can be omitted
 
 const adminData = require('./routes/admin');
