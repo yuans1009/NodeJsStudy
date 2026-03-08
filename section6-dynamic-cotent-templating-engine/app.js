1af36a9cfd71e7e99160676ebedc253414120ac1
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-08 11:46:48 +0800

    Add and move code to controllers

--- section6-dynamic-cotent-templating-engine/app.js
+++ section6-dynamic-cotent-templating-engine/app.js
@@ -3,23 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorCotnroller = require("./controllers/error");
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views'); // views is the default value, can be omitted
 
-const adminData = require('./routes/admin');
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404)
-       .render('404', { pageTitle: 'Page Not Found' });
-});
-
+app.use(errorCotnroller.get404);
+   
 app.listen(3000);
