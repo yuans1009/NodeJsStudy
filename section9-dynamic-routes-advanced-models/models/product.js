ce4df515240a7cbd8e03d53c0965908936a398e5
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-22 11:35:49 +0800

    Define Product model using Sequelize

--- section9-dynamic-routes-advanced-models/models/product.js
+++ section9-dynamic-routes-advanced-models/models/product.js
@@ -1,55 +1,29 @@
-const db = require("../util/database");
-const Cart = require("./cart");
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    // Use VALUES(?, ?, ?, ?) to prevent SQL injection
-    return db.execute(
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description],
-    );
-  }
-
-  static deleteById(id) {
-    if (!id) {
-      return;
-    }
-
-    getProductsFromFile((products) => {
-      if (!products) {
-        console.log("products not found");
-        return;
-      }
-      const product = products.find((p) => p.id === id);
-      if (!product) {
-        console.log("product not found");
-        return;
-      }
-
-      const updatedProducts = products.filter((p) => p.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        if (!err) {
-          Cart.deleteProductById(id, product.price);
-        }
-
-        console.log(err);
-      });
-    });
-  }
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id, cb) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-};
+module.exports = Product;
