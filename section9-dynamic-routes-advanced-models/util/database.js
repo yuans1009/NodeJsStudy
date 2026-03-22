4de06fc756ad8c876fa7ddb2987cbeafdd3ea005
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-03-22 11:30:45 +0800

    Connecting to DB using Sequelize

--- section9-dynamic-routes-advanced-models/util/database.js
+++ section9-dynamic-routes-advanced-models/util/database.js
@@ -1,12 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-// don't create new connection for every request, but create a pool of connections
-
-const pool = mysql.createPool({
-  host: 'localhost', // or '127.0.0.1' if preferred
-  user: 'user',
-  database: 'node-complete',
-  password: 'userpassword',
+const sequelize = new Sequelize("node-complete", "user", "userpassword", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
