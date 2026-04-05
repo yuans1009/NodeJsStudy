a4ece30705b375355b03f4028ce8d3d580ad38ef
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-04-05 13:02:04 +0800

    Clear cart. Get orders.

--- section9-dynamic-routes-advanced-models/controllers/shop.js
+++ section9-dynamic-routes-advanced-models/controllers/shop.js
@@ -169,9 +169,11 @@ exports.deleteCartItem = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+   let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -190,6 +192,9 @@ exports.postOrder = (req, res, next) => {
         });
     })
     .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
       res.redirect("/orders");
     })
     .catch((err) => {
@@ -198,10 +203,17 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  // eager loading products for orders
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
