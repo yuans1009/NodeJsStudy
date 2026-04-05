85af48f8cd938ff7929e8b1a3483cbdccd562e55
yuan-shih-seagull-com <yshih@seagullscientific.com>
2026-04-05 12:47:57 +0800

    create-order button, route, and endpoint

--- section9-dynamic-routes-advanced-models/controllers/shop.js
+++ section9-dynamic-routes-advanced-models/controllers/shop.js
@@ -1,5 +1,4 @@
-const Product = require('../models/product');
-const Cart = require("../models/cart");
+const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -169,16 +168,45 @@ exports.deleteCartItem = (req, res, next) => {
     });
 };
 
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            }),
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
   });
 };
--- section9-dynamic-routes-advanced-models/routes/shop.js
+++ section9-dynamic-routes-advanced-models/routes/shop.js
@@ -19,6 +19,8 @@ router.post("/cart", shopController.postCart);
 
 router.post("/cart-delete-item", shopController.deleteCartItem);
 
+router.post("/create-order", shopController.postOrder);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
